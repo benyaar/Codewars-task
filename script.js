c89e7d46d9f60e78385246018de0613f9cc940ed
Artur <ghostmatt36@gmail.com>
2022-06-28 21:12:57 +0300

    4-5

--- script.js
+++ script.js
@@ -34,5 +34,32 @@ function duplicateEncode(str){
 console.log(duplicateEncode('BaRaban'));
 
 //---3
-function lovefunc(flower1, flower2){
+function lovefunc(flower1, flower2) {
     return flower1 % 2 !== flower2 % 2;
+}
+//---4
+
+function longestConsec(strarr, k) {
+    if (strarr.length==0 || k>strarr.length || k<=0){
+        return "";
+    }else{
+        var list =[];
+        for (var i=0;i<strarr.length-k+1;i++)
+        {
+            list[i]=strarr.slice(i,k+i).join('');
+        }
+
+        var longest = list.reduce(function (a, b) { return a.length >= b.length ? a : b; });
+        return longest;
+    }
+}
+//---5
+function paperwork(n,m){
+
+    if (n<0 || m<0) {
+        return 0;
+    }
+    else{
+        return n*m;
+    }
+}
